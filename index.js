a74b3696b6bca12df5aad53e6951f0dc67306042
RyanLE <ryan.le@efrei.net>
2024-12-04 23:53:00 +0100

    Serveur Express qui marche

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Test depuis Node.js")
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json()); // Pour traiter les données JSON
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Bonjour je suis Ryan ! Bienvenue sur mon API REST simple!' });
+});
+
+app.listen(port, () => {
+  console.log(`Serveur démarré : http://localhost:${port}`);
+});
