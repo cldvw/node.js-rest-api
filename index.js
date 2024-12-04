6699f669beb8f1ecd122ad41410c6185e9e0cffb
RyanLE <ryan.le@efrei.net>
2024-12-05 00:16:32 +0100

    Route POST et Route GET en place

--- index.js
+++ index.js
@@ -2,20 +2,29 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.json()); // Pour traiter les données JSON
+// Middleware pour traiter le JSON
+app.use(express.json());
 
+// Données fictives
+const users = [
+  { id: 1, firstName: "Ryan", lastName: "LE", role: "Admin" },
+  { id: 2, firstName: "Michel", lastName: "Smith", role: "User" },
+];
+
+// Route GET
 app.get('/users', (req, res) => {
-    res.json(users);
-  });
+  res.json(users);
+});
 
-  const users = [
-    { id: 1, firstName: "John", lastName: "Doe", role: "Admin" },
-    { id: 2, firstName: "Jane", lastName: "Smith", role: "User" },
-  ];
-  
+// Route POST
+app.post('/users', (req, res) => {
+  console.log('Body reçu:', req.body); // Debug
+  const newUser = req.body;
+  users.push({ id: users.length + 1, ...newUser });
+  res.status(201).json(newUser);
+});
 
+// Lancer le serveur
 app.listen(port, () => {
   console.log(`Serveur démarré : http://localhost:${port}`);
 });
-
-  
