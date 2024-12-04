3158cec3460aa3298042c0be4ff01b2fdc3087d8
RyanLE <ryan.le@efrei.net>
2024-12-05 00:00:57 +0100

    Récupération GET pour une liste de d'utilisateurs (http://localhost:3000/users)

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ const port = 3000;
 
 app.use(express.json()); // Pour traiter les données JSON
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Bonjour je suis Ryan ! Bienvenue sur mon API REST simple!' });
-});
+app.get('/users', (req, res) => {
+    res.json(users);
+  });
+
+  const users = [
+    { id: 1, firstName: "John", lastName: "Doe", role: "Admin" },
+    { id: 2, firstName: "Jane", lastName: "Smith", role: "User" },
+  ];
+  
 
 app.listen(port, () => {
   console.log(`Serveur démarré : http://localhost:${port}`);
 });
+
+  
